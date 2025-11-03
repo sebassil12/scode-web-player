0ae47a014a9d602d5e43b18983cc24b96ac5bdc4
sebassil12 <anddysilva15@gmail.com>
2025-11-02 22:05:52 -0500

    DEL: non supported style

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
           inter.variable,
         )}
       >
-        <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+        <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur">
           <div className="container flex h-14 items-center">
             <MainNavbar />
           </div>
